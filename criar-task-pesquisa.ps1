0a20629a9b2d5f40351e8943bf192d49641a1095
caiobholanda <caiobholanda2007@gmail.com>
2026-05-12 12:32:58 -0300

    auto: 2026-05-12 12:32:58

--- criar-task-pesquisa.ps1
+++ criar-task-pesquisa.ps1
@@ -2,6 +2,5 @@ $vault = "C:\Users\estagio.ti\Desktop\ClaudeCode\SegundoCerebroOficial"
 $action   = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$vault\pesquisa-diaria.ps1`""
 $trigger  = New-ScheduledTaskTrigger -Daily -At "10:00"
 $settings = New-ScheduledTaskSettingsSet -StartWhenAvailable -ExecutionTimeLimit (New-TimeSpan -Minutes 30)
-$principal = New-ScheduledTaskPrincipal -UserId "estagio.ti" -LogonType Interactive -RunLevel Highest
-Register-ScheduledTask -TaskName "cerebro-pesquisa" -Action $action -Trigger $trigger -Settings $settings -Principal $principal -Description "Pesquisa diaria automatica do Segundo Cerebro" -Force
+Register-ScheduledTask -TaskName "cerebro-pesquisa" -Action $action -Trigger $trigger -Settings $settings -Description "Pesquisa diaria automatica do Segundo Cerebro" -Force
 Write-Host "Task cerebro-pesquisa criada com sucesso!"
